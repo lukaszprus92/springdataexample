1098229c527d7ca293cce2cc43dda4b2a9135b87
Prus <lukaszprus92@gmail.com>
2020-03-26 19:44:27 +0100

    2

--- src/main/java/pl/bykowski/springdataexample/Start.java
+++ src/main/java/pl/bykowski/springdataexample/Start.java
@@ -5,7 +5,10 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Table;
+
 @Component
+@Table(name="huj")
 public class Start {
 
     private CarRepo carRepo;
@@ -19,15 +22,17 @@ public class Start {
     @EventListener(ApplicationReadyEvent.class)
     public void runExample() {
 
-        Car car1 = new Car("Honda", "Jazz", Color.BLUE);
-        carRepo.save(car1);
+        //Car car1 = new Car("Honda", "Jazz", Color.BLUE);
+        //carRepo.save(car1);
 
         Car car2 = new Car("Audi", "A1", Color.BLACK);
         carRepo.save(car2);
 
-        Car car3 = new Car("Audi", "A2", Color.RED);
-        carRepo.save(car3);
+        //Car car3 = new Car("Audi", "A2", Color.RED);
+        //carRepo.save(car3);
 
+        //Car car4 = new Car("huj", "hwdp", Color.RED);
+        //carRepo.save(car4);
 
        // Iterable<Car> all2 = carRepo.findAll();
        // all2.forEach(System.out::println);
